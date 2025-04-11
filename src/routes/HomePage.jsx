a4eb47d5bcf808d6fbfd20cd070a968d0adcf470
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-12 00:16:33 +0100

    first commit

---
+++ src/routes/HomePage.jsx
@@ -0,0 +1,21 @@
+// useEffect(()=>{
+//     const url = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     accept: 'application/json',
+//     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NDM1OTIxZTcyMDA2YThmMmZjNTFlODE3NDQ5NDliNyIsIm5iZiI6MTczNzkyMTAyNC40NzIsInN1YiI6IjY3OTY5MjAwZDRiYTE3MjVmMTJiMDk4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Jq7iJxzZLJjx_l7oQOwRAQOCXcnBhbakxNuc-228wbY'
+//   }
+// };
+
+
+function HomePage() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default HomePage
+
