7e64fb74af7fde9429187be89967f5e8f5c4899d
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-12 12:09:13 +0100

    removed unwanted dependencies

---
+++ utilities/ThemeButton.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react"
+import { ThemeProvider } from "@emotion/react"
+
+function ThemeButton() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ThemeButton
