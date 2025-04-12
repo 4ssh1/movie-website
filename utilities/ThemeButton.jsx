ffa3c6a48e8ed39b9a03bf2f3e7c60d12600dec7
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-12 12:26:19 +0100

    minor change

--- utilities/ThemeButton.jsx
+++ utilities/ThemeButton.jsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react"
-import { ThemeProvider } from "@emotion/react"
+import { ThemeProvider, Typography, createTheme } from "@mui/material";
+import {Button, Typography } from "@mui/material";
+
 
 function ThemeButton() {
+    const [darkMode, setDarkMode] = useState(true)
+
   return (
     <div>
       
     </div>
   )
 }
+console.log(require.resolve('@mui/material/styles'));
 
 export default ThemeButton
