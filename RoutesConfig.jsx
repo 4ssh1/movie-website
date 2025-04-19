b564a089b556291e8b5854d5908da1c39b7cc2cd
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-19 08:52:37 +0100

    made changes to directories

--- RoutesConfig.jsx
+++ RoutesConfig.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './src/components/Navbar/Navbar';
 import mainRoutes from './src/routes/MainRoutes';
 import searchRoutes from './src/routes/SearchRoutes';
-import detailsRoutes from './src/routes/DetalsRoutes';
+import detailsRoutes from './src/routes/DetailsRoutes';
 import Layout from './src/components/Layout';
 
 const allRoutes = [...mainRoutes, ...searchRoutes, ...detailsRoutes];
