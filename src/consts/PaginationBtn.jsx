6b69a5eca62e0df416724140516c849b9484c978
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-19 08:37:02 +0100

    added more pages and routes

--- src/consts/PaginationBtn.jsx
+++ src/consts/PaginationBtn.jsx
@@ -1,11 +1,7 @@
 import { useSearchParams } from 'react-router-dom';
 import { usePages } from '../../utilities/PaginationCxtProv';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
-function PaginationBtn() {
+function PaginationBtn({currentPage = 1, pages = 20}) {
     const {count, setCount} = usePages()
     const [searchParams, setSearchParams] = useSearchParams()
 
@@ -15,14 +11,10 @@ function PaginationBtn() {
     }
 
   return (
-    <div className=' flex justify-center pb-10 '>
-        <Pagination count={10}
-        page={count}
-        onChange={()=>handleChange(e)}
-        renderItem={(item) => (
-            <PaginationItem className='hover:bg-slate-300 dark:text-white'
-             slots={{ previous: ArrowBackIcon , next: ArrowForwardIcon }} {...item} />
-        )}/>
+    <div className=' flex justify-center pb-10 gap-3 text-white text-sm'>
+       <button className='bg-slate-600 px-2 py-1 rounded-md paytone cursor-pointer hover:bg-slate-500'>Prev</button>
+       <button className='bg-slate-600 px-2 py-1 rounded-md paytone cursor-pointer hover:bg-slate-500'>Next</button>
+       <button className='bg-slate-600 px-2 py-1 rounded-md paytone cursor-pointer hover:bg-slate-500'>{currentPage} of {pages}</button>
     </div>
   )
 }
