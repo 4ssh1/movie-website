a4061ec18998d7f06d235743a9a4ba68c6f227fe
Nzeshi Sarah <sarahnzeshi05@gmail.com>
2025-04-21 22:41:13 +0100

    updated footer design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,12 +6,14 @@ function Footer() {
   return (
     <div className="relative sm:h-[40vh] h-[30vh] bg-[url('/collage.jpg')]">
         <div className="absolute bg-slate-800 -z-10  w-full h-full"></div>
-        <div className='backdrop-blur-[4px] h-full flex flex-col  justify-center items-center gap-2'>
+        <div className='backdrop-blur-[4px] h-full flex flex-col  justify-center items-center gap-4'>
             {[0,3,6,9].map((no)=>(
             <div key={no} className="text-white
-               leading-10 [&>div>p]:cursor-pointer sm:font-bold space-x-20">
+               leading-10 roboto text-xs tracking-wider space-x-20">
                 {links.slice(no, no + 3).map((link)=>(
-                    <Link to={'/'} key={link} className='bg-slate-500 '>{link}</Link>
+                    <Link to={'/'} key={link} className=' bg-slate-800 cursor-pointer px-4 
+                    py-2 rounded-full hover:opacity-60 ease-in'
+                    >{link}</Link>
                 ))}
             </div>
                ))}
@@ -19,5 +21,7 @@ function Footer() {
     </div>
   )
 }
+
+// [&>div>p]: a select format in tailwind
 
 export default Footer
